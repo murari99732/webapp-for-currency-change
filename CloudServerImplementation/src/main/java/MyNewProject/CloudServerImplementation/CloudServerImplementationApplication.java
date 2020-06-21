514c5a232be92e28450f1877c53c21e5c30bc8ee
Murari Kumar <murari.kumar@tieto.com>
2020-06-22 02:39:55 +0530

    Config service for different properties are added to use like qa and dev env

--- CloudServerImplementation/src/main/java/MyNewProject/CloudServerImplementation/CloudServerImplementationApplication.java
+++ CloudServerImplementation/src/main/java/MyNewProject/CloudServerImplementation/CloudServerImplementationApplication.java
@@ -2,8 +2,10 @@ package MyNewProject.CloudServerImplementation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudServerImplementationApplication {
 
 	public static void main(String[] args) {
