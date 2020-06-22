45f9f71176ebc745b61e33d0fee266716e5fd7e0
Murari Kumar <murari.kumar@tieto.com>
2020-06-23 02:24:59 +0530

    Port issue fixed autowired added

--- exchange-service/src/main/java/myfirstproject/exchangeservice/ExchangeController.java
+++ exchange-service/src/main/java/myfirstproject/exchangeservice/ExchangeController.java
@@ -2,6 +2,7 @@ package myfirstproject.exchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExchangeController {
 
-	
+	@Autowired
 	Environment environment;
 	
 	@GetMapping("/exchange/from/{from}/to/{to}")
